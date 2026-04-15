4322f17c28ea476251c3d039b865b40951c39e75
Saurabh Aditya <s.adityalal@gmail.com>
2026-04-15 19:03:28 +0200

    Initial Commit

---
+++ DesktopDog/DesktopDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesktopDogApp.swift
+//  DesktopDog
+//
+//  Created by Aditya Saurabh on 15.04.26.
+//
+
+import SwiftUI
+
+@main
+struct DesktopDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
